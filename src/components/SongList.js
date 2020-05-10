67e7c2c294d8f40f7c5fc4a1afed085112c85c48
vlad0k <vladok.sha089@gmail.com>
2020-05-10 18:33:33 +0200

    song list componen't

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className={"song-list"}>
+      <ul>
+        <li>This wild darkness</li>
+        <li>Memory gospel</li>
+      </ul>
+    </div>
+  )
+}
+ export default SongList;
